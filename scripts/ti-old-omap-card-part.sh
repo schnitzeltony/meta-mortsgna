34904adb9d64a92be2ef3fafc11f34b15ea85ac9
Andreas Müller <schnitzeltony@gmail.com>
2018-10-10 17:44:19 +0200

    ti-old-omap-card-part: Fix some late-night crap
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- scripts/ti-old-omap-card-part.sh
+++ scripts/ti-old-omap-card-part.sh
@@ -9,7 +9,7 @@
 
 # includes
 . `dirname $0`/include/common-helpers.inc
-. `dirname $0`/../tools.inc
+. `dirname $0`/include/card-helpers.inc
 
 
 # overrride default SelectInOut - we have nothing to deploy here
@@ -24,7 +24,7 @@ RootCardWriteCallback() {
     CheckPartitionTable "$DevicePath"
 
     # kill u-boot environment
-    EvalExAuto "dd if=/dev/zero of=$DevicePath ti-old-omap-card-part.sh bs=1024 count=1024" "\nKill u-boot environment..."
+    EvalExAuto "dd if=/dev/zero of=$DevicePath bs=1024 count=1024" "\nKill u-boot environment..."
 
     # Create the FAT partition of 64MB and make it bootable
     EvalExAuto "parted -s $DevicePath mklabel msdos && parted -s $DevicePath mkpart primary fat32 63s 64MB && parted -s $DevicePath toggle 1 boot" "\nCreate boot partition..."
@@ -40,6 +40,6 @@ RootCardWriteCallback() {
 CheckPrerequisite "parted"
 CheckPrerequisite "dd"
 CheckPrerequisite "mkfs.vfat"
-CheckPrerequisite "mkfs.vfat"
+CheckPrerequisite "mke2fs.vfat"
 
 StartCardWrite
